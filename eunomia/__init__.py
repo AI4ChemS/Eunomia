195a8f7f16dd5027becad12f8ff86a7f8ab80a62
mehradans92 <mehrad.ans@gmail.com>
2023-08-11 11:50:49 -0400

    added agent notebook with evaluation, still parsing not withing the agent

--- eunomia/__init__.py
+++ eunomia/__init__.py
@@ -1,9 +1,10 @@
 """Top-level package for Eunomia."""
 
 __author__ = """Mehrad Ansari"""
-__email__ = 'mehrad.ans@gmail.com'
-__version__ = '0.1.0'
+__email__ = "mehrad.ans@gmail.com"
+__version__ = "0.1.0"
 
 from .docs_utils.docs import *
-from .general import *
 from .eunomia import *
+from .general import *
+from .parser import *
